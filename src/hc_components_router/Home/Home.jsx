abc59b5425145f33a1e27a8e32ff2e07c162028c
RiteshWadhwani <119737525+riteshwadhwani@users.noreply.github.com>
2024-02-15 11:36:51 +0530

    Update Home.jsx

--- src/hc_components_router/Home/Home.jsx
+++ src/hc_components_router/Home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
             <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
                 <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
                     <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
-                        <h2 className="text-4xl font-bold text-slate-600  sm:text-slate-200  sm:text-5xl">
+                        <h2 className="text-4xl font-bold text-slate-600 sm:text-5xl">
                             Subhash Baviskar
                             
                         </h2>
